a487a66ee5fee81b3d0081d6167f3067e84adbdd
tekamyashwant <yashwant.tekam22@gmail.com>
2024-08-24 07:13:48 +0530

    recaptcha added

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ function beforeSubmit() {
   let formatteddate = new Date(inputdate.value).toLocaleDateString("en-US");
   outputdate.value = formatteddate;
 }
+
+function timestamp() {
+  var response = document.getElementById("g-recaptcha-response");
+  if (response == null || response.value.trim() == "") {
+    var elems = JSON.parse(
+      document.getElementsByName("captcha_settings")[0].value
+    );
+    elems["ts"] = JSON.stringify(new Date().getTime());
+    document.getElementsByName("captcha_settings")[0].value =
+      JSON.stringify(elems);
+  }
+}
+setInterval(timestamp, 500);
