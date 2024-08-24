5e01a0f9b2ce1a125f67f7aca18080c6a6dd79e6
tekamyashwant <yashwant.tekam22@gmail.com>
2024-08-24 07:43:19 +0530

    recaptcha validations

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-function beforeSubmit() {
-  let outputdate = document.querySelector(".outputdate");
-  let inputdate = document.querySelector(".inputdate");
-  console.log("inputdate", inputdate.value);
+let captchaChecked = false;
 
-  //convert string inputdate into date(en_US)
-  let formatteddate = new Date(inputdate.value).toLocaleDateString("en-US");
-  outputdate.value = formatteddate;
+function beforeSubmit(event) {
+  if (captchaChecked) {
+    let outputdate = document.querySelector(".outputdate");
+    let inputdate = document.querySelector(".inputdate");
+    console.log("inputdate", inputdate.value);
+
+    //convert string inputdate into date(en_US)
+    let formatteddate = new Date(inputdate.value).toLocaleDateString("en-US");
+    outputdate.value = formatteddate;
+  } else {
+    alert("Please check captcha before submit");
+    event.preventDefault();
+  }
 }
 
 function timestamp() {
@@ -20,3 +27,7 @@ function timestamp() {
   }
 }
 setInterval(timestamp, 500);
+
+function successCaptcha() {
+  captchaChecked = true;
+}
